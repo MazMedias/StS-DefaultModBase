8cb972b71ba4ae64eb89153a3737a6e22837809a
Dylan Coffey <MazMedias@gmail.com>
2020-09-15 21:15:35 -0400

    werk

---
+++ theDefault/src/main/java/theUnbound/patches/UnboundTags.java
@@ -0,0 +1,15 @@
+package theUnbound.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public class UnboundTags {
+    @SpireEnum public static AbstractCard.CardTags WOOD;
+    @SpireEnum public static AbstractCard.CardTags FIRE;
+    @SpireEnum public static AbstractCard.CardTags EARTH;
+    @SpireEnum public static AbstractCard.CardTags METAL;
+    @SpireEnum public static AbstractCard.CardTags WATER;
+    @SpireEnum public static AbstractCard.CardTags ENLIGHTENED;
+    @SpireEnum public static AbstractCard.CardTags CORRUPT;
+    @SpireEnum public static AbstractCard.CardTags STANCE;
+}
